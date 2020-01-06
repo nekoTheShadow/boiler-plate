504af0d7e2e8d93b41e77b09e830b3a807139507
nekoTheShadow <h.nakamura0903@gmail.com>
2020-01-06 16:03:11 +0900

    REFACTORING: move some functions to util.go

---
+++ util.go
@@ -0,0 +1,29 @@
+package main
+
+func Min(a ...int) int {
+	v := a[0]
+	for i := 1; i < len(a); i++ {
+		if a[i] < v {
+			v = a[i]
+		}
+	}
+	return v
+}
+
+func Max(a ...int) int {
+	v := a[0]
+	for i := 1; i < len(a); i++ {
+		if v < a[i] {
+			v = a[i]
+		}
+	}
+	return v
+}
+
+func Abs(x int) int {
+	if x > 0 {
+		return x
+	} else {
+		return x * -1
+	}
+}

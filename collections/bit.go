fa88e59e4f9027eba6bc3540c84bb9d8a907b397
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2020-03-01 17:45:23 +0900

    Binary Indexed Tree

---
+++ collections/bit.go
@@ -0,0 +1,31 @@
+package collections
+
+// Binary Indexed Tree (1 based indexed)
+type BIT struct {
+	size int
+	tree []int
+}
+
+func NewBIT(size int) *BIT {
+	bit := BIT{
+		size: size,
+		tree: make([]int, size+1),
+	}
+	return &bit
+}
+
+func (bit *BIT) Sum(i int) int {
+	sum := 0
+	for i > 0 {
+		sum += bit.tree[i]
+		i -= i & -i
+	}
+	return sum
+}
+
+func (bit *BIT) Add(i, x int) {
+	for i <= bit.size {
+		bit.tree[i] += x
+		i += i & -i
+	}
+}

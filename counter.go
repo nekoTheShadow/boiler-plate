fbf9274ad4db94f812f1f94222eabb0ba22bbee4
nekoTheShadow <h.nakamura0903@gmail.com>
2020-04-21 11:46:30 +0900

    counter: Decrementを削除 & リファクタリング

--- counter.go
+++ counter.go
@@ -11,20 +11,9 @@ func NewCounter() *Counter {
 	return &Counter{}
 }
 
-func (c *Counter) put(key interface{}, v int) {
-	d := map[interface{}]int(*c)
-	d[key] = v
-}
-
 func (c *Counter) Increment(key interface{}) {
-	c.put(key, c.Get(key)+1)
-}
-
-func (c *Counter) Decrement(key interface{}) {
-	v := c.Get(key)
-	if v > 0 {
-		c.put(key, v-1)
-	}
+	d := map[interface{}]int(*c)
+	d[key] = c.Get(key) + 1
 }
 
 func (c *Counter) Get(key interface{}) int {
@@ -40,9 +29,7 @@ func (c *Counter) Keys() []interface{} {
 	d := map[interface{}]int(*c)
 	keys := []interface{}{}
 	for key := range d {
-		if c.Get(key) > 0 {
-			keys = append(keys, key)
-		}
+		keys = append(keys, key)
 	}
 	return keys
 }
